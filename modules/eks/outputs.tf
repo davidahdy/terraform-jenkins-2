db5fc1e11ef133ff4d47fffa1e790a55720b17f0
mohamed55979 <72396071+mohamed55979@users.noreply.github.com>
2025-11-14 08:42:25 +0200

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -47,6 +47,38 @@ output "cluster_arn" {
   value       = aws_eks_cluster.main.arn
 }
 
+# modules/eks/outputs.tf
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.main.name
+}
+
+output "cluster_id" {
+  description = "The ID of the EKS cluster"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = aws_eks_cluster.main.arn
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
 output "oidc_provider_arn" {
   description = "ARN of the OIDC Provider for EKS"
   value       = aws_eks_cluster.main.identity[0].oidc[0].issuer
