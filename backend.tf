7153e9f80f1ad8c5dbdc689aebbf1eae24de5d68
davidahdy <105285667+davidahdy@users.noreply.github.com>
2025-11-21 20:34:36 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,9 @@
 terraform {
-  required_version = ">= 1.5.0"
-
-  # Enable the lockfile feature
-  use_lockfile = true
-
   backend "s3" {
     bucket  = "hello-app-s3-jenkins"
     key     = "eks/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
+   # use_lockfile = true   
   }
 }
